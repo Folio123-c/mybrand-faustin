5886be594af2f88323261beae5f295457615e1b2
Folio123-c <niyof97@gmail.com>
2023-03-11 14:36:28 +0200

    Draft blogs  updated

--- js/addblog.js
+++ js/addblog.js
@@ -8,9 +8,6 @@ form.addEventListener('submit', handleSubmit);
 saveDraftBtn.addEventListener('click', handleSaveDraft);
 publishBtn.addEventListener('click', handlePublish);
 
-// Define the local storage keys
-// const PUBLISHED_POSTS_KEY = 'published_posts';
-// const DRAFT_POSTS_KEY = 'draft_posts';
 
 // Define the handlers for the form and buttons
 function handleSubmit(event) {
@@ -42,11 +39,7 @@ function handlePublish(event) {
     const title = form.elements['name'].value;
     const content = form.elements['message'].value;
     const image = form.elements['image'].value;
-    // const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDlkMjdiNWFhZDE0Nzg3MmU3ZjEwNiIsImlhdCI6MTY3ODM2NTMzNX0.vGsjwZYPgcS3G4lDmSVjGDvtv7Dr-_QdzLhkS73IOtk'
-    // const hfj = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDlkMjdiNWFhZDE0Nzg3MmU3ZjEwNiIsImlhdCI6MTY3ODM2NTMzNX0.vGsjwZYPgcS3G4lDmSVjGDvtv7Dr-_QdzLhkS73IOtk'
-    // const cookies = {
-    //     token: userToken
-    // }
+
     // Store the post as published
     publishPost({ title, image, content});
     // Clear the form
@@ -99,14 +92,9 @@ async function saveDraft(post) {
     }
 }
 
-// function getDraftPosts() {
-//     return JSON.parse(localStorage.getItem(DRAFT_POSTS_KEY)) || [];
-// }
 
 function publishPost(post) {
-    // const publishedPosts = getPublishedPosts();
-    // publishedPosts.push(post);
-    // localStorage.setItem(PUBLISHED_POSTS_KEY, JSON.stringify(publishedPosts));
+
     const token = JSON.parse(localStorage.getItem('token'));
     fetch("https://mybrand-faustin.cyclic.app/api/v1/blogs", {
         method: "POST",
@@ -128,19 +116,4 @@ function publishPost(post) {
     alert("Blog published Successfully");
 }
 
-// function getPublishedPosts() {
-//     return JSON.parse(localStorage.getItem(PUBLISHED_POSTS_KEY)) || [];
-// }
-//
-//
-// let title= document.querySelectorAll('#blog-title');
-// let content= document.querySelectorAll('#blog-p')
-//
-// let allblogs=JSON.parse(localStorage.getItem('draft_posts'))
-// console.log("allblogs");
-// console.log(allblogs);
-// for(let i=0; i< title.length; i++){
-//     title[i].innerHTML=allblogs[i].title;
-//     content[i].innerHTML=allblogs[i].content;
-//
-// }
+
